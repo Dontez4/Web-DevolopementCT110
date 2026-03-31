2c23a7861f492871dd098e7c8fcbf1d23521bf3f
Dontez4 <dantekurz1@gmail.com>
2026-03-31 19:35:44 -0400

    Add files via upload

---
+++ P1LAB1_KurzDante.py.py
@@ -0,0 +1,10 @@
+#Dante Kurz
+#3/30/2026
+#P1LAB1 - Python Basics
+#This program will ask the user for their name and then greet them.
+
+# Get the user's name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
+print()
